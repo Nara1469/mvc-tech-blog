839e1691c4df3c7635fb3dd42ff8fb87e98ce106
Nara Davaasuren <naraamtm@gmail.com>
2022-04-16 09:33:24 -0600

    Changed create and update views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
       });
     // } else {
     //   res.render('homepage', {
-    //     posts,
+    //     posts, 
     //     // comments,
     //     logged_in: false
     //   });
